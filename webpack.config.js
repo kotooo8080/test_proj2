7b50f3f5fe2fb9e6fa1595b04fb08922eccc2861
kotooo8080 <kotooo8080@gmail.com>
2024-06-22 11:11:55 +0300

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: 'index.html',
       chunks: ['main']
     })
   ].concat(multipleHtmlPlugins),
