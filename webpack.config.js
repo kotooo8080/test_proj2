b0ebd14e271d9b646e194954152c99c29e5a00c7
kotooo8080 <kotooo8080@gmail.com>
2024-06-22 10:35:03 +0300

    config fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,50 @@
-const path = require('path');
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+let htmlPageNames = ['page2', 'page3'];
+let multipleHtmlPlugins = htmlPageNames.map(name => {
+  return new HtmlWebpackPlugin({
+    template: `./${name}.html`,
+    filename: `${name}.html`,
+    chunks: ['main']
+  })
+});
+
 module.exports = {
-    mode: 'development',
-    entry: './src/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.(scss|css)$/, // Регулярное выражение для обработки файлов с расширением .css, .scss
-            use: ['style-loader', 'css-loader', 'sass-loader'], // Загрузчики, используемые для обработки CSS/Sass - файлов
-          },
-        ],
+  entry: {
+    main: './src/js/index.js'
+  }, // Точка входа для сборки проекта
+
+  output: {
+    filename: 'bundle.js', // Имя выходного файла сборки
+    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/, // Регулярное выражение для обработки файлов с расширением .css, .scss
+        use: ['style-loader', 'css-loader', 'sass-loader'], // Загрузчики, используемые для обработки CSS/Sass - файлов
       },
-    plugins: [
-        new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: 'index.html',
-          chunks: ['main']
-        }),
-        new HtmlWebpackPlugin({
-          filename: 'page2.html',
-          template: 'page2.html',
-          chunks: ['main']
-        }),
-        new HtmlWebpackPlugin({
-          filename: 'page3.html',
-          template: 'page3.html',
-          chunks: ['main']
-        })
-      ],
+    ],
+  },
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      chunks: ['main']
+    })
+  ].concat(multipleHtmlPlugins),
 
-    devServer: {
-        port: 8080,
-        hot: "only",
-        static: {
-          directory: path.join(__dirname, './'), // Каталог для статики
-          serveIndex: true,
-        },
-        open: true, // Автоматически открывать браузер
-      }, 
-}
+  devServer: {
+    port: 8080,
+    hot: "only",
+    static: {
+      directory: path.join(__dirname, './'), // Каталог для статики
+      serveIndex: true,
+    },
+    open: true, // Автоматически открывать браузер
+  }, 
+
+  mode: 'development', // Режим сборки
+};
